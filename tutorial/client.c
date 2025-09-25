4a53a203cf220b6f74a40638c9e1b6855af255f3
Changyue Tan <tanchangyue1997@gmail.com>
2025-09-25 15:35:36 +1000

    part4 unfinihsed with error: vmm microkit_ppcall: invalid channel given '1'

--- tutorial/client.c
+++ tutorial/client.c
@@ -1,30 +1,25 @@
-#include <microkit.h>
-#include <stdbool.h>
 #include <stdint.h>
-#include <string.h>
-
+#include <stdbool.h>
+#include <microkit.h>
 #include "printf.h"
 #include "wordle.h"
 
-#define MOVE_CURSOR_UP              "\033[5A"
+#define MOVE_CURSOR_UP "\033[5A"
 #define CLEAR_TERMINAL_BELOW_CURSOR "\033[0J"
-#define GREEN                       "\033[32;1;40m"
-#define YELLOW                      "\033[39;103m"
-#define DEFAULT_COLOUR              "\033[0m"
 
 #define INVALID_CHAR (-1)
 
-#define CHANNEL_WITH_SERIAL_SERVER_ID 1
-#define CHANNEL_WITH_WORDLE_SERVER_ID 2
+#define CHANNEL_TO_SERIAL_SERVER 1
+#define CHANNEL_TO_WORDLE_SERVER 2
 
 #define WORDLE_WORD_SIZE 5
 
-char *input_buffer_base_vaddr;
-char *output_buffer_base_vaddr;
+char *input_buffer; // handle user input
+char *output_buffer; // display game state
 
 struct wordle_char {
-	int ch;
-	enum character_state state;
+    int ch;
+    enum character_state state;
 };
 
 // Store game state
@@ -35,18 +30,18 @@ static int curr_row = 0;
 static int curr_letter = 0;
 
 void wordle_server_send() {
-	// Implement this function to send the word over PPC
-	// After doing the PPC, the Wordle server should have updated
-	// the message-registers containing the state of each character.
-	// Look at the message registers and update the `table` accordingly.
+    // Implement this function to send the word over PPC
+    // After doing the PPC, the Wordle server should have updated
+    // the message-registers containing the state of each character.
+    // Look at the message registers and update the `table` accordingly.
 
-	microkit_msginfo message_structure_to_be_passed = microkit_msginfo_new(0, WORDLE_WORD_SIZE);
+    microkit_msginfo message_structure_to_be_passed = microkit_msginfo_new(0, WORDLE_WORD_SIZE);
     // put word entered by user into message registers
     for (int i = 0; i < WORDLE_WORD_SIZE; i++) {
         microkit_mr_set(i, table[curr_row][i].ch);
     }
     // call worldle server's protected procedure and get return message
-    microkit_msginfo message_structure_returned_from_PP = microkit_ppcall(CHANNEL_WITH_WORDLE_SERVER_ID, message_structure_to_be_passed);
+    microkit_msginfo message_structure_returned_from_PP = microkit_ppcall(CHANNEL_TO_WORDLE_SERVER, message_structure_to_be_passed);
     // update the table with character states
     for (int i = 0; i < WORDLE_WORD_SIZE; i++) {
         table[curr_row][i].state = microkit_mr_get(i);
@@ -54,109 +49,112 @@ void wordle_server_send() {
 }
 
 void serial_send(char *str) {
-	// Implement this function to get the serial server to print the string.
-	for (int i = 0; str[i] != '\0'; i++) {
-		output_buffer_base_vaddr[i] = str[i];
-	}
-	microkit_notify(
-	    CHANNEL_WITH_SERIAL_SERVER_ID);  // tell serial server that buffer is ready
+    // Implement this function to get the serial server to print the string.
+    for (int i = 0; str[i] != '\0'; i++) {
+        output_buffer[i] = str[i];
+    }
+    // printf("HEY!");
+    microkit_notify(CHANNEL_TO_SERIAL_SERVER); // tell serial server that buffer is ready   
 }
 
 // This function prints a CLI Wordle using pretty colours for what characters
 // are correct, or correct but in the wrong place etc.
 void print_table(bool clear_terminal) {
-	if (clear_terminal) {
-		// Assuming we have already printed a Wordle table, this will clear the
-		// table we have already printed and then print the updated one. This
-		// is done by moving the cursor up 5 lines and then clearing everything
-		// below it.
-		serial_send(MOVE_CURSOR_UP);
-		serial_send(CLEAR_TERMINAL_BELOW_CURSOR);
-	}
+    if (clear_terminal) {
+        // Assuming we have already printed a Wordle table, this will clear the
+        // table we have already printed and then print the updated one. This
+        // is done by moving the cursor up 5 lines and then clearing everything
+        // below it.
+        serial_send(MOVE_CURSOR_UP);
+        serial_send(CLEAR_TERMINAL_BELOW_CURSOR);
+    }
 
-	for (int row = 0; row < NUM_TRIES; row++) {
-		for (int letter = 0; letter < WORD_LENGTH; letter++) {
-			serial_send("[");
-			enum character_state state = table[row][letter].state;
-			int ch = table[row][letter].ch;
-			if (ch != INVALID_CHAR) {
-				switch (state) {
-					case INCORRECT: break;
-					case CORRECT_PLACEMENT: serial_send(GREEN); break;
-					case INCORRECT_PLACEMENT: serial_send(YELLOW); break;
-					default:
-						// Print out error messages/debug info via debug output
-						microkit_dbg_puts(
-						    "CLIENT|ERROR: unexpected character state\n");
-				}
-				char ch_str[] = {ch, '\0'};
-				serial_send(ch_str);
-				// Reset colour
-				serial_send(DEFAULT_COLOUR);
-			} else {
-				serial_send(" ");
-			}
-			serial_send("] ");
-		}
-		serial_send("\n");
-	}
+    for (int row = 0; row < NUM_TRIES; row++) {
+        for (int letter = 0; letter < WORD_LENGTH; letter++) {
+            serial_send("[");
+            enum character_state state = table[row][letter].state;
+            int ch = table[row][letter].ch;
+            // printf("state of %c is %d\n", ch, state);
+            if (ch != INVALID_CHAR) {
+                switch (state) {
+                    case INCORRECT: break;
+                    case CORRECT_PLACEMENT: serial_send("\x1B[32m"); break;
+                    case INCORRECT_PLACEMENT: serial_send("\x1B[33m"); break;
+                    default:
+                        // Print out error messages/debug info via debug output
+                        microkit_dbg_puts("CLIENT|ERROR: unexpected character state\n");
+                }
+                char ch_str[] = { ch, '\0' };
+                serial_send(ch_str);
+                // Reset colour
+                serial_send("\x1B[0m");
+            } else {
+                serial_send(" ");
+            }
+            serial_send("] ");
+        }
+        serial_send("\n");
+    }
 }
 
 void init_table() {
-	for (int row = 0; row < NUM_TRIES; row++) {
-		for (int letter = 0; letter < WORD_LENGTH; letter++) {
-			table[row][letter].ch = INVALID_CHAR;
-			table[row][letter].state = INCORRECT;
-		}
-	}
+    for (int row = 0; row < NUM_TRIES; row++) {
+        for (int letter = 0; letter < WORD_LENGTH; letter++) {
+            table[row][letter].ch = INVALID_CHAR;
+            table[row][letter].state = INCORRECT;
+        }
+    }
 }
 
 bool char_is_backspace(int ch) {
-	return (ch == 0x7f);
+    return (ch == 0x7f);
 }
 
 bool char_is_valid(int ch) {
-	// Only allow alphabetical letters and do not accept a character if the
-	// current word has already been filled.
-	return ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) &&
-	       curr_letter != WORD_LENGTH;
+    // Only allow alphabetical letters and do not accept a character if the
+    // current word has already been filled.
+    return ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) && curr_letter != WORD_LENGTH;
 }
 
 void add_char_to_table(char c) {
-	if (char_is_backspace(c)) {
-		if (curr_letter > 0) {
-			curr_letter--;
-			table[curr_row][curr_letter].ch = INVALID_CHAR;
-		}
-	} else if (c != '\r' && c != ' ' && curr_letter != WORD_LENGTH) {
-		table[curr_row][curr_letter].ch = c;
-		curr_letter++;
-	}
+    if (char_is_backspace(c)) {
+        if (curr_letter > 0) {
+            curr_letter--;
+            table[curr_row][curr_letter].ch = INVALID_CHAR;
+        }
+    } else if (c != '\r' && c != ' ' && curr_letter != WORD_LENGTH) {
+        table[curr_row][curr_letter].ch = c;
+        curr_letter++;
+    }
 
-	// If the user has finished inputting a word, we want to send the
-	// word to the server and move the cursor to the next row.
-	if (c == '\r' && curr_letter == WORD_LENGTH) {
-		wordle_server_send();
-		curr_row += 1;
-		curr_letter = 0;
-	}
+    // If the user has finished inputting a word, we want to send the
+    // word to the server and move the cursor to the next row.
+    if (c == '\r' && curr_letter == WORD_LENGTH) {
+        wordle_server_send();
+        curr_row += 1;
+        curr_letter = 0;
+    }
 }
 
 void init(void) {
-	microkit_dbg_puts("CLIENT: starting\n");
-	serial_send("Welcome to the Wordle client!\n");
+    microkit_dbg_puts("CLIENT: starting\n");
+    serial_send("Welcome to the Wordle client!\n");
 
-	init_table();
-	// Don't want to clear the terminal yet since this is the first time
-	// we are printing it (we want to clear just the Wordle table, not
-	// everything on the terminal).
-	print_table(false);
+    init_table();
+
+    // Don't want to clear the terminal yet since this is the first time
+    // we are printing it (we want to clear just the Wordle table, not
+    // everything on the terminal).
+
+    print_table(false);
 }
 
 void notified(microkit_channel channel) {
-	switch (channel) {
-		case CHANNEL_WITH_SERIAL_SERVER_ID:
-			add_char_to_table(*input_buffer_base_vaddr);
-			print_table(true);
-	}
+    switch (channel) {
+        case CHANNEL_TO_SERIAL_SERVER:
+            // microkit_dbg_puts("Received message from server!\n");
+            add_char_to_table(input_buffer[0]);
+            print_table(true);
+    }
+
 }
--- tutorial/wordle_server.c
+++ tutorial/wordle_server.c
@@ -1,12 +1,12 @@
-#include <microkit.h>
-#include <stdbool.h>
-#include <stddef.h>
 #include <stdint.h>
-
+#include <stdbool.h>
+#include <microkit.h>
+#include <stddef.h>
 #include "printf.h"
 #include "wordle.h"
 
-#define CHANNEL_WITH_CLIENT_ID 2
+#define CHANNEL_TO_CLIENT 1
+#define CHANNEL_TO_VMM 2
 
 #define WORDLE_WORD_SIZE 5
 
@@ -14,64 +14,69 @@
  * Here we initialise the word to "hello", but later in the tutorial
  * we will actually randomise the word the user is guessing.
  */
-char word[WORD_LENGTH] = {'h', 'e', 'l', 'l', 'o'};
+char word[WORD_LENGTH] = { 'h', 'e', 'l', 'l', 'o' };
 
 bool is_character_in_word(char *word, int ch) {
-	for (int i = 0; i < WORD_LENGTH; i++) {
-		if (word[i] == ch) {
-			return true;
-		}
-	}
+    for (int i = 0; i < WORD_LENGTH; i++) {
+        if (word[i] == ch) {
+            return true;
+        }
+    }
 
-	return false;
+    return false;
 }
 
 enum character_state char_to_state(int ch, char *word, uint64_t index) {
-	if (ch == word[index]) {
-		return CORRECT_PLACEMENT;
-	} else if (is_character_in_word(word, ch)) {
-		return INCORRECT_PLACEMENT;
-	} else {
-		return INCORRECT;
-	}
+    if (ch == word[index]) {
+        return CORRECT_PLACEMENT;
+    } else if (is_character_in_word(word, ch)) {
+        return INCORRECT_PLACEMENT;
+    } else {
+        return INCORRECT;
+    }
 }
 
 void init(void) {
-	microkit_dbg_puts("WORDLE SERVER: starting\n");
+    microkit_dbg_puts("WORDLE SERVER: starting\n");
 }
 
 microkit_msginfo protected(microkit_channel channel, microkit_msginfo msginfo) {
-	microkit_msginfo message_structure_to_be_returned =
-	    microkit_msginfo_new(0, WORDLE_WORD_SIZE); // lable = 0; size = 5
 
-	switch (channel) {
-		case CHANNEL_WITH_CLIENT_ID:
-			int entered_word[WORDLE_WORD_SIZE];
-			int entered_word_states[WORDLE_WORD_SIZE];
-			// get the word passed from the message registers
-			for (int i = 0; i < WORDLE_WORD_SIZE; i++) {
-				entered_word[i] = microkit_mr_get(i);
-			}
-			// check if the word passed from the messages registers are correct
-			for (int i = 0; i < WORDLE_WORD_SIZE; i++) {
-				entered_word_states[i] =
-				    char_to_state(entered_word[i], word, i);
-			}
-			// return the states back to the caller of this protection procedure
-			for (int i = 0; i < WORDLE_WORD_SIZE; i++) {
-				microkit_mr_set(i, entered_word_states[i]);
-			}
-			break;
-		default:
-			break;
-	}
-	return message_structure_to_be_returned;
+    microkit_msginfo message_structure_to_be_returned = microkit_msginfo_new(0, 0);
+
+    switch (channel) {
+        case CHANNEL_TO_CLIENT: 
+            int entered_word[WORDLE_WORD_SIZE];
+            int entered_word_states[WORDLE_WORD_SIZE];
+            // get the word passed from the message registers
+            for (int i = 0; i < WORDLE_WORD_SIZE; i++) {
+                entered_word[i] = microkit_mr_get(i);
+            }
+            // check if the word passed from the messages registers are correct
+            for (int i = 0; i < WORDLE_WORD_SIZE; i++) {
+                entered_word_states[i] = char_to_state(entered_word[i], word, i);
+            }
+            // return the states back to the caller of this protection procedure
+            message_structure_to_be_returned = microkit_msginfo_new(0, WORDLE_WORD_SIZE);
+            for (int i = 0; i < WORDLE_WORD_SIZE; i++) {
+                microkit_mr_set(i, entered_word_states[i]);
+            }
+            break;
+        case CHANNEL_TO_VMM:
+            int update_word_from_remote_server[WORDLE_WORD_SIZE];
+            // update the word array to the random new word
+            for (int i = 0; i < WORDLE_WORD_SIZE; i++) {
+                word[i] = microkit_mr_get(i);
+            }
+            break;
+    }
+    return message_structure_to_be_returned;
 }
 
 void notified(microkit_channel channel) {
-	switch (channel) {
-		case CHANNEL_WITH_CLIENT_ID:
-			microkit_dbg_puts("WORDLE SERVER: Received message from client!\n");
-			break;
-	}
+    switch (channel) {
+        case CHANNEL_TO_CLIENT: 
+            microkit_dbg_puts("WORDLE SERVER: Received message from client!\n");
+            break;
+    }
 }
